4e7f733d6297b6511baf4240186ecf2860a8f3d6
Munabilac <munabilacintya@outlook.com>
2023-11-25 02:21:43 +0700

    assignment 3

---
+++ main.js
@@ -0,0 +1,31 @@
+import Typed from "typed.js";
+import confetti from "canvas-confetti";
+
+const typed = new Typed("#element", {
+  strings: [
+    "Beginner Programmer.",
+    "Software Engineer wannabe.",
+    "Devscale student.",
+  ],
+  typeSpeed: 50,
+});
+
+const appreciateBtn = document.getElementById("appreciate");
+appreciateBtn.addEventListener("click", () => {
+  const myCanvas = document.createElement("canvas");
+  myCanvas.style.position = "fixed";
+  myCanvas.style.inset = 0;
+  myCanvas.style.width = "100vw";
+  myCanvas.style.height = "100vh";
+  myCanvas.style.zIndex = -1;
+  document.body.appendChild(myCanvas);
+
+  const myConfetti = confetti.create(myCanvas, {
+    resize: true,
+    useWorker: true,
+  });
+  myConfetti({
+    particleCount: 100,
+    spread: 160,
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        navy: "#2700FF",
+      },
+    },
+  },
+  plugins: [],
+};
